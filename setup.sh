a62c2b398f6f1642b27412372819464caa9467c0
Josef Blažek <blaza004@gmail.com>
2022-05-12 18:03:59 +0200

    5

--- setup.sh
+++ setup.sh
@@ -15,6 +15,16 @@ makepkg -si --noconfirm
 sudo yay -Syu --noconfirm
 
 # install dependencies
+sudo pacman -S polybar --noconfirm
+sudo pacman -S terminator --noconfirm
+sudo pacman -S neofetch --noconfirm
+sudo pacman -S lxappearance --noconfirm
+sudo pacman -S material-gtk-theme --noconfirm
+sudo pacman -S papirus-icon-theme --noconfirm
+sudo pacman -S arc-icon-theme
+sudo pacman -S rofi --noconfirm
+sudo pacman -S picom --noconfirm
+sudo pacman -S i3-lock --noconfirm
 
 # download and load scripts
 cd ~
@@ -23,4 +33,6 @@ sudo rm -R ~/.config/polybar
 sudo rm -R ~/.config/scripts
 sudo rm -R ~/.config/gtk-3.0
 sudo rm -R ~/.config/rofi
+sudo rm -R ~/.config/picom
+sudo rm -R ~/.config/terminator
 sudo cp -R $BASEDIR/* ~/.config 
